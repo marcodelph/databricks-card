16345ba47797919a289141f031a766a969581f07
marcodelph <marcodelph@hotmail.com>
2025-08-04 06:29:54 -0300

    adição do databricks.yml com actions

--- scripts/produce_anomaly.py
+++ scripts/produce_anomaly.py
@@ -8,6 +8,7 @@ from faker import Faker
 from dotenv import load_dotenv
 from azure.eventhub.aio import EventHubProducerClient
 from azure.eventhub import EventData
+import datetime
 
 load_dotenv()
 
@@ -32,15 +33,15 @@ personas = {
     "2002": {"min_amount": 800, "max_amount": 2500},
     "2005": {"min_amount": 1000, "max_amount": 3500},
 }
-# Lista de IDs de usuário para sorteio
 user_ids = list(personas.keys())
 
-# Anomalia
-# O usuário '1001' (Econômico) fará uma compra com o perfil do '2002' (Gastador)
 ANOMALY_TARGET_USER = "1001"
 ANOMALY_BEHAVIOR_SOURCE_USER = "2002"
+current_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=2)
 
 async def run():
+    global current_time
+
     producer = EventHubProducerClient.from_connection_string(
         conn_str=CONNECTION_STR,
         eventhub_name=EVENT_HUB_NAME
@@ -50,21 +51,22 @@ async def run():
             transaction = {}
             anomaly_type = "none"
             
-            # Decide se a transação será uma anomalia injetada (3% de chance)
+            # Avança o tempo para a próxima transação
+            current_time += datetime.timedelta(seconds=random.randint(1800, 14400)) # Entre 30 min e 4 horas
+            
             if random.random() < 0.03:
                 anomaly_type = "ANOMALIA CONTEXTUAL"
                 user_id = ANOMALY_TARGET_USER
                 source_persona = personas[ANOMALY_BEHAVIOR_SOURCE_USER]
                 amount = round(random.uniform(source_persona["min_amount"], source_persona["max_amount"]), 2)
             else:
-                # Gera uma transação normal baseada em uma das personas
                 user_id = random.choice(user_ids)
                 persona = personas[user_id]
                 amount = round(random.uniform(persona["min_amount"], persona["max_amount"]), 2)
 
             transaction = {
                 'transaction_id': str(uuid.uuid4()),
-                'timestamp_utc': str(fake.iso8601()),
+                'timestamp_utc': current_time.isoformat(),
                 'user_id': user_id,
                 'card_number': fake.credit_card_number(),
                 'amount_brl': amount,
@@ -82,7 +84,7 @@ async def run():
             else:
                 print(f"Transação normal enviada -> Usuário: {transaction['user_id']} | Valor: R$ {transaction['amount_brl']:.2f}")
 
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(0.25)
 
 if __name__ == "__main__":
     asyncio.run(run())
